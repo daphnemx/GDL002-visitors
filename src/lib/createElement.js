35b3cc3d7b8a43f12b76195a0be25444f8ad5d44
ClaudiaGarfias <me@claudiagarfias.works>
2019-04-20 11:18:42 -0500

    Primer test

---
+++ src/lib/createElement.js
@@ -0,0 +1,15 @@
+const createElement = (tagName, opts = {}) => {
+  const { children, ...rest } = opts;
+  const element = Object.assign(
+    document.createElement(tagName),
+    rest,
+  );
+
+  if (children && typeof children.forEach === 'function') {
+    children
+      .filter(item => item)
+      .forEach(element.appendChild.bind(element));
+  }
+
+  return element;
+};
---
+++ test/lib/createElement.test.js
@@ -0,0 +1,7 @@
+const createElement = require('../../src/lib/createElement');
+
+describe('createElement', () => {
+  it('is an object', () => {
+    expect(typeof createElement).toBe('object');
+  });
+});
