77199b23d736504d075f13b4089219371180a6df
ClaudiaGarfias <me@claudiagarfias.works>
2019-04-21 09:54:07 -0500

    Se añadió firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+	apiKey: "AIzaSyATd_NEeul67on2O3YhMTmgVmjFmKtYKm8",
+	authDomain: "visitors-registration.firebaseapp.com",
+	databaseURL: "https://visitors-registration.firebaseio.com",
+	projectId: "visitors-registration",
+	storageBucket: "visitors-registration.appspot.com",
+	messagingSenderId: "573685055395"
+});
