ffa786d408d4870279b52e0d69ded07c666edf0d
ClaudiaGarfias <me@claudiagarfias.works>
2019-04-21 12:09:04 -0500

    Se añadió pantalla principal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const contentDiv = document.getElementById('root');
 contentDiv.classList.add('flex-column-log');
 
-<<<<<<< HEAD
 if (location.hash === '') {
 	const a = createElement('button', { innerText: 'Registro de Visitantes', className: 'getVisitors main-screen-btn' });
 	contentDiv.innerHTML = a.outerHTML;
@@ -32,27 +31,6 @@ else if (location.hash === '#visitors') {
 		.catch(function(error) {
 		    console.error("Error adding document: ", error);
 		});
-=======
-const loadPhotoBtn = document.querySelector('.loadPhotoBtn');
-const loadRegistryBtn = document.querySelector('.registerBtn');
-const photo = document.querySelector ('.load-img');
-
-loadRegistryBtn.addEventListener('click', () => {
-	firebase.firestore().collection("visitorsLog").add(
-		visitRegistry = { date: getDate(),
-		time: getTime(),
-		visitComments : document.querySelector('.visitComments').value,
-		visitCompany : document.querySelector('.visitCompany').value,
-		visitEmail : document.querySelector('.visitEmail').value,
-		visitHost : document.querySelector('.visitHost').value,
-		visitName : document.querySelector('.visitName').value,
-		visitPhoto : photo.src }).
-	then(function(docRef) {
-	    console.log("Document written with ID: ", docRef.id, visitRegistry);
-	})
-	.catch(function(error) {
-	    console.error("Error adding document: ", error);
->>>>>>> 2899bd643996e2bab474337ca2c1599c4a60ea10
 	});
 }
 
