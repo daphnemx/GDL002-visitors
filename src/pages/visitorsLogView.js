9f6eccfff0facbc11236e6469c3df738391d0f96
ClaudiaGarfias <me@claudiagarfias.works>
2019-05-02 02:11:45 -0500

    estilo select & inputs

--- src/pages/visitorsLogView.js
+++ src/pages/visitorsLogView.js
@@ -9,19 +9,36 @@ const visitorsLogView = () => {
 				createElement('input', { className: 'visitEmail' }),
 				createElement('label', { innerText: 'Empresa u Organización a la que Pertences:' }),
 				createElement('input', { className: 'visitCompany' }),
-				createElement('label', { innerText: '¿Quién es tu anfitrión?' }),
-				createElement('select', { className: 'visitHost', children:
+				
+
+				createElement('div', { className: 'selects', children:[
+createElement('div', { className: 'left-select', children: [
+	createElement('label', { innerText: '¿Quién es tu anfitrión?' }),
+					createElement('select', { className: 'visitHost', children:
 					[createElement('option', { value: '', innerText: 'Elige:' }),
 					createElement('option', { value: 'patriciaga@protonmail.com', innerText: 'Patricia Garfias' }),
-					createElement('option', { value: 'stuff@claudiagarfias.works', innerText: 'Otro Coworker' })] }),
-				createElement('label', { innerText: '¿Cuál es tu Tiempo de Visita Máximo Estimado?' }),
+					createElement('option', { value: 'stuff@claudiagarfias.works', innerText: 'Otro Coworker' })] })
+	] }),
+
+createElement('div', { children: [
+createElement('label', { innerText: 'Tiempo de Visita Máx:' }),
 				createElement('select', { className: 'visitMaxTime', children:
 					[createElement('option', { value: '00:30', innerText: '00:30 min' }),
 					createElement('option', { value: '01:00', innerText: '01:00 hrs' }),
 					createElement('option', { value: '01:30', innerText: '01:30 hrs' }),
 					createElement('option', { value: '02:00', innerText: '02:00 hrs' }),
 					createElement('option', { value: '02:30', innerText: '02:30 hrs' }),
-					createElement('option', { value: '03:00', innerText: '03:00 hrs' })] }),
+					createElement('option', { value: '03:00', innerText: '03:00 hrs' })] })
+	] })
+
+
+				
+
+					]}),
+
+				
+
+
 				createElement('label', { innerText: 'Comentarios:' }),
 				createElement('input', { className: 'visitComments' })],
 		});
