f715933604886bf537804858af2ece460ede6c8c
ClaudiaGarfias <me@claudiagarfias.works>
2019-05-02 08:35:43 -0500

    cambio de logo

--- src/pages/mainView.js
+++ src/pages/mainView.js
@@ -4,7 +4,7 @@ const mainView = () => {
 	const menuSection = createElement('section', { className: 'main-section', children: [
 		createElement('div', { className: 'logo-section', children: [
 		createElement('img', { className: 'main-logo', src:'http://claudiagarfias.works/contents/uploads/media/easylog.png' }),
-		createElement('h2', { innerText:'Visitors Log Made Easy' })] }),
+		createElement('h2', { innerText:'Visitors  Log  Made  Easy' })] }),
 		createElement('div', {className: 'main-buttons flex-row-main-screen', children:[
 			createElement('button', { innerText: 'Administración', className: 'getAdmin main-screen-btn' }),
 			createElement('button', { innerText: 'Registrar Visitantes', className: 'getVisitors main-screen-btn' }),
