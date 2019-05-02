d35110aaab0ca6b1be6a1481b3c66417b090ea04
ClaudiaGarfias <me@claudiagarfias.works>
2019-05-01 23:48:33 -0500

    Resolved responsive main screen

--- src/pages/mainView.js
+++ src/pages/mainView.js
@@ -1,12 +1,10 @@
 const backgroundVid = createElement('video', { className: 'background-vid', autoplay : true, muted: true, loop: true, src: 'https://claudiagarfias.works/contents/uploads/media/coworking-background-video.mp4' });
 
 const mainView = () => {
-	// const logoSection = createElement('section', { className: 'logo-section',Z children: [
-	// 	createElement('h1', { innerText: 'Registro de Visitantes' })
-	// 	], });
 	const menuSection = createElement('section', { className: 'main-section', children: [
 		createElement('div', { className: 'logo-section', children: [
-		createElement('img', { className: 'main-logo', src:'http://claudiagarfias.works/contents/uploads/media/easylog.png' })] }),
+		createElement('img', { className: 'main-logo', src:'http://claudiagarfias.works/contents/uploads/media/easylog.png' }),
+		createElement('h2', { innerText:'Visitors Log Made Easy' })] }),
 		createElement('div', {className: 'main-buttons flex-row-main-screen', children:[
 			createElement('button', { innerText: 'Administración', className: 'getAdmin main-screen-btn' }),
 			createElement('button', { innerText: 'Registrar Visitantes', className: 'getVisitors main-screen-btn' }),
@@ -14,8 +12,5 @@ const mainView = () => {
 			]})
 		
 		], });
-	// backgroundVid.playbackRate = .25;
-
 	return menuSection;
-
 }
